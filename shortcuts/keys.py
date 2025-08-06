e707a1b4ca93e58a6be6db15b3ce68a5cad9a3d0
ramz0 <luis.syber@gmail.com>
2025-08-06 12:30:41 -0600

    Cambiar tamaño de teclas con flechas [usa un Layout Especifico].

--- shortcuts/keys.py
+++ shortcuts/keys.py
@@ -22,12 +22,11 @@ llaves = [
     Key([mod], "Down", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod], "Up", lazy.layout.shuffle_up(), desc="Move window up"),
     
-    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key(
-        [mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"
-    ),
-    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
-    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    # Cambiar Tamaño [Para un Layout especifico].
+    Key([mod, "control"], "Left", lazy.layout.grow_left(), desc="Grow window to the left"),
+    Key([mod, "control"], "Right", lazy.layout.grow_right(), desc="Grow window to the right"),
+    Key([mod, "control"], "Down", lazy.layout.grow_down(), desc="Grow window down"),
+    Key([mod, "control"], "Up", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
     Key(
         [mod, "shift"],
@@ -35,13 +34,17 @@ llaves = [
         lazy.layout.toggle_split(),
         desc="Toggle between split and unsplit sides of stack",
     ),
-    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-    # Toggle between different layouts as defined below
+
+    # Cambiar de layout.
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+
+    # Acciones del sistema.
     Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    #  ~ Rofi.
+    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    
+    # Rofi.
     Key(
         [mod],
         "r",
@@ -68,7 +71,7 @@ llaves = [
 
     #  Abrir VSCode.
     Key([mod], "c", lazy.spawn("code"), desc="Open visual-studio-code"),
-    
+
     # Volumen
     Key([], "XF86AudioMute", lazy.spawn("pamixer --toggle-mute")),
     Key([], "XF86AudioLowerVolume", lazy.spawn("pamixer --decrease 2")),
