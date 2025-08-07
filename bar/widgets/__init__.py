828f9abda2ead067b3cf3fe7e979df3c54ab9309
ramz0 <luis.syber@gmail.com>
2025-08-06 18:42:04 -0600

    Implementacion de widgets Blocks.

--- bar/widgets/__init__.py
+++ bar/widgets/__init__.py
@@ -1,7 +1,9 @@
 from bar.widgets.basics import widgets as basic_widgets
 from bar.widgets.customs import widgets as custom_widgets
+from bar.widgets.blocks import blocks as widgets_blocks
 
 widgets = {
     **basic_widgets,
     **custom_widgets,
+    **widgets_blocks,
 }
