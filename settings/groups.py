4d43871d80d98b3371d0c30b6e6c72cf6645970e
ramz0 <luis.syber@gmail.com>
2025-08-08 09:40:51 -0600

    keys importadas

--- settings/groups.py
+++ settings/groups.py
@@ -1,29 +1,4 @@
-from libqtile.config import  Group, Key
-from libqtile.lazy import lazy
-from shortcuts.keys import llaves, mod
+from libqtile.config import Group
 
-keys = llaves
-
+# Solo defines los grupos
 grupos = [Group(i) for i in ["1", "2", "3", "4", "5", "6"]]
-
-# 󰡙 󰡘 󰡜 󰡗 󰡚 󰡛      綠  祿     󱓞 󰫣     󰇧    ⏾    󰧞 󰧟 󰆘 󰷀
-
-for i, group in enumerate(grupos):
-    print('index: ', i, ' grupo: ', group, ' Nombre del grupo: ', group.name)
-    index = str(i + 1)
-    keys.extend(
-        [
-            Key(
-                [mod],
-                index,
-                lazy.group[group.name].toscreen(toggle=True),
-                desc="Switch to groups {}".format(group.name),
-            ),
-            Key(
-                [mod, "shift"],
-                index,
-                lazy.window.togroup(group.name, switch_group=True),
-                desc="Switch to & move focused window to groups {}".format(group.name),
-            ),
-        ]
-    )
