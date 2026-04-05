76e5e433af382c814b2f34838bfb6a61a8f0d634
ramz0 <luis.syber@gmail.com>
2026-04-04 21:36:06 -0600

    Agregar segunda barra para monitor externo con widgets groupbox

--- bar/bar.py
+++ bar/bar.py
@@ -1,8 +1,9 @@
 import os
-from libqtile import bar
+from libqtile import bar, widget
 from libqtile.config import Screen
 
 from theme.colors import *
+from theme.decorations import *
 
 from bar.widgets import widgets
 
@@ -26,20 +27,41 @@ bar_widgets = [
     *widgets["battery"],
     widgets["small_spacer"],
     *widgets["clock"],
-]
+],
+
+bar_widgets_secondary = [
+    widgets["small_spacer"],
+    *widgets["groupbox"],
+    widgets["big_spacer"], widgets["small_spacer"],
+    *widgets["clock"],
+    widgets["sep"],
+    widgets["softwarename"],
+    widgets["sep"],
+    *widgets["clock"],
+    widgets["small_spacer"],
+],
 
 # Creacion de la Barra del sistema.
 bar = [
     Screen(
         wallpaper=os.path.expanduser("~/documents/wallpapers/starwars.png"),
-        # Podemos cambiar a [top, bottom].
         top=bar.Bar(
             bar_widgets,
             20,
             margin=[3, 3, 0, 3],
             background=colorBarra,
-            border_width=4,  # Draw top and bottom borders
-            border_color=colorBarra,  # Borders are magenta
+            border_width=4,
+            border_color=colorBarra,
+        ),
+    ),
+    Screen(
+        top=bar.Bar(
+            bar_widgets_secondary,
+            20,
+            margin=[3, 3, 0, 3],
+            background=colorBarra,
+            border_width=4,
+            border_color=colorBarra,
         ),
     ),
 ]
