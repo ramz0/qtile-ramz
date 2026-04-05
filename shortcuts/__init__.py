c923eb19a2f0c5f70c01d2196238ef526557a112
ramz0 <luis.syber@gmail.com>
2026-04-04 21:36:04 -0600

    Agregar atajos de monitores al modulo de shortcuts

--- shortcuts/__init__.py
+++ shortcuts/__init__.py
@@ -3,6 +3,7 @@ from .keys_groups import keys as group
 from .keys_rofi import keys as rofi
 from .keys_apps import keys as apps
 from .keys_media import keys as media
+from .keys_monitors import keys as monitors
 
 
-keys = general + group + rofi + apps + media
+keys = general + group + rofi + apps + media + monitors
