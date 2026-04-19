473131fcc94c182d653c4025a404b551b6918220
ramz0 <luis.syber@gmail.com>
2026-04-18 20:52:29 -0600

    shortcuts rofi

--- shortcuts/keys_rofi.py
+++ shortcuts/keys_rofi.py
@@ -8,4 +8,7 @@ keys = [
     Key(["mod1"], "Tab", lazy.spawn('rofi -show window -icon-theme "Papirus" -show-icons')),
     Key([mod], "e", lazy.spawn("rofi -show emoji")),
     Key([mod], "p", lazy.spawn(".config/rofi/bin/powermenu")),
+    Key(["control", "shift"], "s", lazy.spawn(".config/rofi/bin/screenshot")),
+    Key([mod], "d", lazy.spawn(".config/rofi/bin/calendar")),
+    Key([mod, "shift"], "n", lazy.spawn(".config/rofi/bin/wifi")),
 ]
